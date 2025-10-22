fixed normalisation and better plots

MVD_reconstrucion/data/get_data.py
@@ -1,6 +1,4 @@
-import random
 import pandas as pd
-import os
 import numpy as np
 
 from sklearn.model_selection import train_test_split
@@ -11,8 +9,11 @@ import tensorflow as tf
 
 
 def process_data_tensor_flow(file_path: str):
+    """splits data, normalises data
+    issue as it does not normalise each column separately
+    assumes all columns are related"""
+
     data = preprocess_data(pd.read_csv(file_path, sep=";"))
-    data = data.iloc[:, :-1]
     raw_data = data.values  # numpy array
 
     # split data
@@ -31,31 +32,9 @@ def process_data_tensor_flow(file_path: str):
     return train_data, test_data
 
 
-def singleStepSampler(df: pd.DataFrame, window: int) -> (np.ndarray, np.ndarray):
-    """Prepares the data for single-step time-series forecasting"""
-    xRes = []  # store input features
-    yRes = []  # store target values
-
-    # create sequences of input features and corresponding target values based on window size
-    # input features constructed as a sequence of windowed data points,
-    # where each data point is a list containing values from each column of the dataframe
-    for i in range(0, len(df) - window):
-        res = []
-        for j in range(0, window):
-            r = []
-            for col in df.columns:
-                r.append(df[col][i + j])
-            res.append(r)
-        xRes.append(res)
-        # filter output columns here if needed
-        yRes.append(df.iloc[i + window].values)  # filter columns here
-    return np.array(xRes), np.array(yRes)
-
-
 def process_data_scaling(file_path: str):
     """
-    TODO:
-        fix this
+    imputes missing values, scales columns independently, splits data
     """
     data = preprocess_data(pd.read_csv(file_path, sep=";"))
 
@@ -73,19 +52,12 @@ def process_data_scaling(file_path: str):
     # target_scalar = MinMaxScaler(feature_range=(0, 1))
 
     df_scaled = df_scaled.astype(float)
+    raw_scaled_data = df_scaled.values
 
-    # apply singleStepSample with window size of 20
-    (xVal, yVal) = singleStepSampler(df_scaled, window=20)
+    # split data
+    train_data, test_data = train_test_split(raw_scaled_data, test_size=0.2, random_state=21)
 
-    # a constant split with a value of 0.85 is defined, specifying proportion of data to be used for training
-    # xVal and yVal are split into training and testing sets according to the split ratio.
-    # training set has 0.85% of data, testing has 0.15%
-    SPLIT = 0.85
-
-    x_train = xVal[:int(SPLIT * len(xVal))]
-    y_train = yVal[:int(SPLIT * len(yVal))]
-    x_test = xVal[int(SPLIT * len(xVal)):]
-    y_test = yVal[int(SPLIT * len(yVal)):]
+    return train_data, test_data
 
 
 def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:

MVD_reconstrucion/tensor_conv_ae.py
@@ -10,7 +10,6 @@ from keras.models import Model
 from data.get_data import *
 
 
-col_to_show = 2  # 0-17
 num_to_show = 50
 
 
@@ -51,42 +50,43 @@ def conv_ae(file_path: str):
     split data, normalise data, build model, train model, reconstruct test_data, plot test_data against reconstruct
     """
 
-    train_data, test_data = process_data_tensor_flow(file_path)
+    # split & normalise data
+    train_data, test_data = process_data_scaling(file_path)
+
+    print(f"{len(test_data[0])} columns in data")
 
     # build model
+    print("building model")
     autoencoder = AnomalyDetector(len(test_data[0]))
     autoencoder.compile(optimizer="adam", loss="mae")
     autoencoder.summary()
 
     # train model
+    print("training model")
     history = autoencoder.fit(train_data, train_data, epochs=20, validation_data=(test_data, test_data), shuffle=True)
 
-    # reconstruct test_data
+    # reconstructing test_data
+    print("reconstructing data")
     encoded_data = autoencoder.encoder(test_data).numpy()
     decoded_data = autoencoder.decoder(encoded_data).numpy()
 
     # plot test data against reconstructed data
-    print(f"{len(test_data[0])} columns")
-    for x in range(0, len(test_data[0]), 2):
-        plt.plot((test_data[:, x])[:num_to_show], label=f"test {x}")
-        plt.plot((decoded_data[:, x])[:num_to_show], label=f"reconstructed {x}")
+    print("plotting test data against reconstructed data")
+    for x in range(0, len(test_data[0])):
+        fig, ax = plt.subplots(figsize=(10, 6))
+        plt.plot((test_data[:, x])[:num_to_show], label=f"test {x}", color="b")
+        plt.plot((decoded_data[:, x])[:num_to_show], label=f"reconstructed {x}", color="r")
+        plt.fill_between(np.arange(len((test_data[:, x])[:num_to_show])), (decoded_data[:, x])[:num_to_show],
+                         (test_data[:, x])[:num_to_show], label="error", color="lightcoral")
 
-        if x+1 < len(test_data[0]):
-            plt.plot((test_data[:, x+1])[:num_to_show], label=f"test {x+1}")
-            plt.plot((decoded_data[:, x+1])[:num_to_show], label=f"reconstructed {x+1}")
-
-        plt.yticks(np.linspace(0, 1, 11))
-        plt.legend()
-        plt.savefig(f"images/plot_{x}_and_{x+1}.png")
+        ax.set_yticks(np.linspace(0, 1, 11))
+        plt.xlabel("Timestamps")
+        plt.ylabel("Normalised values")
+        ax.legend()
+        plt.savefig(f"images/plot_{x}.png")
         plt.cla()
 
-    # plot col_to_show column, with error gap highlighted
-    plt.plot((test_data[:, col_to_show])[:num_to_show], "b")
-    plt.plot((decoded_data[:, col_to_show])[:num_to_show], "r")
-    plt.fill_between(np.arange(num_to_show), (decoded_data[:, col_to_show])[:num_to_show], (test_data[:, col_to_show])[:num_to_show], color="lightcoral")
-    plt.yticks(np.linspace(0, 1, 11))
-    plt.savefig("images/Figure_3.png")
-    plt.cla()
+    print("plots saved to images/")
 
 
 if __name__ == "__main__":