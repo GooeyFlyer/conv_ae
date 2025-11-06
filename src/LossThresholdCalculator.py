f469a80d0a3fb8af92086b3107ee2e543d76410f
GooeyFlyer <garrett.thomas.business@gmail.com>
2025-11-06 11:58:37 +0100

    custom mean_absolute_percentage_error and cosine_similarity to fit with Plotting

--- src/LossThresholdCalculator.py
+++ src/LossThresholdCalculator.py
@@ -6,21 +6,20 @@ import numpy as np
 class LossThresholdCalculator:
     def __init__(self, loss: str, threshold_quantile: float):
 
-        # TODO: make these work with PlottingManager
+        # you can add your own functions here, ensure they conform to f(y_pred, y_true)
         loss_function_dict = {
             "mean_absolute_error": keras.losses.mean_absolute_error,
             "mean_squared_error": keras.losses.mean_squared_error,
-            # "mean_absolute_percentage_error": keras.losses.mean_absolute_percentage_error,
+            "mean_absolute_percentage_error": self.mean_absolute_percentage_error,
             "mean_squared_logarithmic_error": keras.losses.mean_squared_logarithmic_error,
-            # "cosine_similarity": keras.losses.cosine_similarity,
+            "cosine_similarity": self.cosine_similarity,
             "huber": keras.losses.huber,
             "log_cosh": keras.losses.log_cosh,
-            # "tversky": keras.losses.tversky,
-            # "dice": keras.losses.dice
         }
 
         try:
             self.calculate_loss = loss_function_dict[loss]
+            self.loss = loss
 
         except KeyError as e:
             raise KeyError(f"loss ({loss}) not found. Supported options are {loss_function_dict.keys()}")
@@ -33,8 +32,13 @@ class LossThresholdCalculator:
 
     def __call__(self, train_reconstructions, test_reconstructions, reshaped_train_data, reshaped_test_data,
                  *args, **kwargs):
+        """
+        returns loss arrays with values between 0 and 1.
+        use keras loss functions to avoid this
+        """
 
         print("\ncalculating test loss, threshold, & train loss")
+        print(f"using {self.loss} and threshold quantile {self.threshold_quantile}")
 
         train_loss = tf.reshape(
             self.calculate_loss(y_pred=train_reconstructions, y_true=reshaped_train_data),
@@ -50,3 +54,20 @@ class LossThresholdCalculator:
         )
 
         return train_loss, test_loss, threshold
+
+        # modify the result of keras loss functions to fit with PlottingManager
+
+    def mean_absolute_percentage_error(self, y_true, y_pred):
+        """scales result of keras mape to between 0 and 1"""
+        loss = keras.losses.mean_absolute_percentage_error(y_true, y_pred)
+
+        from sklearn.preprocessing import MinMaxScaler
+
+        # feature scaling - ensure they all fall within 0 to 1
+        scalar = MinMaxScaler(feature_range=(0, 1))
+        return scalar.fit_transform(loss.numpy())
+
+    def cosine_similarity(self, y_true, y_pred):
+        """adds 1 to result of keras cosine_similarity"""
+        loss = keras.losses.cosine_similarity(y_true, y_pred)
+        return loss + 1
